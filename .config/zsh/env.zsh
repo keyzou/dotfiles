6784ad0c0044f9062353cdb421b444f50ac4a51c
keyzou <keyzou95@gmail.com>
2024-02-16 23:47:38 +0100

    Automatically launch the ssh agent

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -2,5 +2,6 @@
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 . "$HOME/.asdf/asdf.sh"
+eval $(ssh-agent -s) 1> /dev/null
 fpath=(${ASDF_DIR}/completions $fpath)
 export PATH="$PATH:$HOME/.local/bin"
