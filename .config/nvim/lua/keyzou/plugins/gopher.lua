85ca2c75ce0c287c12593628a0f271be25b9eb14
keyzou <keyzou95@gmail.com>
2024-04-10 17:21:15 +0200

    Add another `gopher` shortcut

--- .config/nvim/lua/keyzou/plugins/gopher.lua
+++ .config/nvim/lua/keyzou/plugins/gopher.lua
@@ -4,6 +4,8 @@ return {
 	config = function(_, opts)
 		require("gopher").setup(opts)
 
+		vim.keymap.set("n", "<leader>gie", "<cmd>GoIfErr<CR>")
+
 		vim.keymap.set("n", "<leader>gsj", "<cmd>GoTagAdd json<CR>")
 		vim.keymap.set("n", "<leader>gst", "<cmd>GoTagAdd toml<CR>")
 	end,
