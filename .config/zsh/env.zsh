92f4d910ed3c2bfad9078ba9770953aa58318559
keyzou <keyzou95@gmail.com>
2024-05-09 11:09:43 +0200

    Add yarn global to path & new tmux panes starting in curr pwd

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -2,6 +2,6 @@
 
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/dev/go
-export PATH="$PATH:$HOME/.local/bin:$GOPATH/bin:$GOROOT/bin"
+export PATH="$PATH:$HOME/.local/bin:$GOPATH/bin:$GOROOT/bin:$(yarn global bin)"
 GPG_TTY=$(tty)
 export GPG_TTY
