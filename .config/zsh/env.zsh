7b0f2baa3e99c83f6c749b6d379f5a7b08c279d3
keyzou <keyzou95@gmail.com>
2024-07-06 13:59:10 +0200

    Tokyo night on most tools

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -4,6 +4,8 @@ export GOROOT=/usr/local/go
 export GOPATH=$HOME/dev/go
 export CGO_ENABLED=1
 
+export TERM="wezterm"
+
 export PATH="$PATH:$HOME/.local/bin:$GOPATH/bin:$GOROOT/bin"
 GPG_TTY=$(tty)
 export GPG_TTY
