a6dfa74879df0f2d75145a9b0b6842c2e41a91f8
Dean Chérif <dean@yoshu.ai>
2025-07-25 21:48:27 +0200

    feat(nvim): Move to LazyVim

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,32 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      ruff = {
+        on_attach = function(client, bufnr_attached)
+          _ = client
+          -- Ruff automatic import organization.
+          LazyVim.format.register({
+            name = "ruff.organize_imports",
+            priority = 50, -- Smaller than Conform's 100.
+            primary = false, -- Conform is primary.
+            format = function(bufnr)
+              if bufnr == bufnr_attached then
+                vim.lsp.buf.code_action({
+                  context = {
+                    only = { "source.organizeImports" },
+                    diagnostics = {},
+                  },
+                  apply = true,
+                })
+              end
+            end,
+            sources = function(_)
+              return { "ruff.organize_imports" } -- Dummy name.
+            end,
+          })
+        end,
+      },
+    },
+  },
+}
